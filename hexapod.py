22764e83328c87da74eeb14448f1764d720224ab
Kris Lopez <unknownlopez101@gmail.com>
2022-05-06 10:29:32 -0400

    added thread for printing error state

--- hexapod.py
+++ hexapod.py
@@ -1,5 +1,6 @@
 import json
 from time import sleep
+import threading
 
 from lx16a_controller import LX16A_BUS_MODIFIED
 
@@ -28,6 +29,9 @@ class SERVO:
         self.bus_link.moveServo(self.id, pos)
         self.curr_angle = pos
 
+    def getServoStatus(self):
+        return self.bus_link.readLedError(self.id)
+
 class LEG:
     def __init__(self, servo_objects):
         self.servos = servo_objects
@@ -46,6 +50,10 @@ class LEG:
         self.servos[0].setPosition(self.curr_angs[0] + arc)
         self.updateCurrAngles()
 
+    def getLegStatus(self):
+        errors = {s.id: s.getServoStatus() for s in self.servos}
+        return errors
+
 class HEXAPOD_BODY:
     def __init__(self, servo_params, bus_link):
         self.leg_objects = {"front_right":None, "front_left":None, "mid_right":None, "mid_left":None, "rear_right":None, "rear_left":None}
@@ -56,6 +64,13 @@ class HEXAPOD_BODY:
                 angs = servo_params["leg_angles"][str(id)]
                 servos.append(SERVO(id, *angs, bus_link))
             self.leg_objects[leg] = LEG(servos)
+
+    def printServoStatus(self):
+        for leg in self.leg_objects.keys():
+            print(leg)
+            error = self.leg_objects[leg].getLegStatus()
+            for id in error.keys():
+                print(id, error[id], "\n")
 
     def changeBodyHeight(self, z):
         for leg in self.leg_objects:
@@ -68,46 +83,48 @@ class HEXAPOD_BODY:
         #lift first group legs, rotate forward, and then lower
         for leg in first_group:
             self.leg_objects[leg].raiseLowerLegParallel(z)
-        sleep(1)
+        sleep(0.5)
         for leg in first_group:
             self.leg_objects[leg].moveLegArc(arc)
-        sleep(1)
+        sleep(0.5)
         for leg in first_group:
             self.leg_objects[leg].raiseLowerLegParallel(z * -1)
-        sleep(1)
+        sleep(0.5)
 
         for leg in second_group:
             self.leg_objects[leg].raiseLowerLegParallel(z)
-        sleep(1)
+        sleep(0.5)
         for leg in first_group:
             self.leg_objects[leg].moveLegArc(arc * -1)
-        sleep(1)
+        sleep(0.5)
         for leg in second_group:
             self.leg_objects[leg].raiseLowerLegParallel(z * -1)
-        sleep(1)
+        sleep(0.5)
 
     def moveInDirection(self, leg_directions, arc, z):
         pass
 
+def printHexapodErrors():
+    while True:
+        main_hexapod.printServoStatus()
+        sleep(1)
+
 
 if __name__ == "__main__":
     lx_bus= LX16A_BUS_MODIFIED(debug = True)
-
     with open('servo_params') as f:
         params = json.load(f)
     main_hexapod = HEXAPOD_BODY(params, lx_bus)
 
-    for i in range(10, 100, 10):
-        main_hexapod.changeBodyHeight(i)
-        sleep(1)
-    
-    sleep(2)
+    status_thread = threading.Thread(target=printHexapodErrors, args=())
+    status_thread.start()
 
-    for i in range(10, 100, 10):
-        main_hexapod.changeBodyHeight(i * -1)
-        sleep(1)
-
-    sleep(2)
-    
+    for i in range(1, 10):
+        main_hexapod.changeBodyHeight(i * 10)
+    sleep(1)
+    for i in range(1, 10):
+        main_hexapod.changeBodyHeight(i * -10)
+    sleep(1)
     for i in range(5):
         main_hexapod.rotateInPlace(20, 20)
+
